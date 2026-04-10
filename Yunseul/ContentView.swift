96057b2be5405094aa60c76a95f202a38f349a23
JaeWoong Eum <wodnd0418@gmail.com>
2026-04-10 09:57:58 +0900

    Initial Commit

---
+++ Yunseul/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Yunseul
+//
+//  Created by wodnd on 4/10/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Yunseul/YunseulApp.swift
@@ -0,0 +1,17 @@
+//
+//  YunseulApp.swift
+//  Yunseul
+//
+//  Created by wodnd on 4/10/26.
+//
+
+import SwiftUI
+
+@main
+struct YunseulApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
