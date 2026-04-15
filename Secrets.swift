806ca9a3f714a3a356415dcd4429fca3f3bc30ba
JaeWoong Eum <wodnd0418@gmail.com>
2026-04-15 09:40:35 +0900

    chore: Secrets.swift gitignore 추가

---
+++ Secrets.swift
@@ -0,0 +1,12 @@
+//
+//  Secrets.swift
+//  Yunseul
+//
+//  Created by wodnd on 4/15/26.
+//
+
+import Foundation
+enum Secrets {
+    static let githubToken = "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"
+    static let discordWebhookURL = "https://discord.com/api/webhooks/1493764206032588954/wLSibCEgJc-8NBv1jxn47N-0GqhCbmoaJCkiSAFxheHsKK3mJX3F1bzj9MWQpBx02IoD"
+}
